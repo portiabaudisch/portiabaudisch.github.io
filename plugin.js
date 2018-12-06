c3f603e0aa3fe601df50c1dac904fb95e8405405
Portia Baudisch <pmb33@miami.edu>
2018-12-05 22:25:46 -0500

    plugin try

---
+++ plugin.js
@@ -0,0 +1,11 @@
+
+ $('.anchor-scroll').anchorScroll({
+    scrollSpeed: 800, // scroll speed
+    offsetTop: 0, // offset for fixed top bars (defaults to 0)
+    onScroll: function () {
+      // callback on scroll start
+    },
+    scrollEnd: function () {
+      // callback on scroll end
+    }
+ });
